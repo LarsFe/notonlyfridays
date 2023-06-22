e4ff88a38fa479b9beadff5c4bdf890b4406144f
Lars Feske <l.feske@live.com>
2023-06-22 18:52:23 +0200

    Update CategoryComponent to fetch DB data

--- app/components/categrories/CategoryComponent.tsx
+++ app/components/categrories/CategoryComponent.tsx
@@ -1,92 +1,37 @@
+'use client';
+
 import styles from './categories.module.scss';
 import Image from 'next/image';
+import { useTranslations } from 'next-intl';
+import { Category } from '@prisma/client';
+import React, { use } from 'react';
+
+const getCategories = () =>
+  fetch(`http://localhost:3000/api/categories`).then((res) => res.json());
 
 export default function CategoryComponent() {
+  const t = useTranslations('Categories');
+  const categories = use<Category[]>(getCategories());
+
   return (
     <div className={styles['category-collection']}>
       <ul className={styles['category-collection__list']}>
-        <li className={styles['category-collection__item']}>
-          <Image
-            src="/images/language.svg"
-            alt="Beleaf Logo"
-            width={40}
-            height={40}
-            priority
-          />
-          <span className={styles['category-collection__category']}>
-            Spielwaren
-          </span>
-        </li>
-        <li className={styles['category-collection__item']}>
-          <Image
-            src="/images/language.svg"
-            alt="Beleaf Logo"
-            width={40}
-            height={40}
-            priority
-          />
-          <span className={styles['category-collection__category']}>
-            Kleidung
-          </span>
-        </li>
-        <li className={styles['category-collection__item']}>
-          <Image
-            src="/images/language.svg"
-            alt="Beleaf Logo"
-            width={40}
-            height={40}
-            priority
-          />
-          <span className={styles['category-collection__category']}>
-            Unverpackt
-          </span>
-        </li>
-        <li className={styles['category-collection__item']}>
-          <Image
-            src="/images/language.svg"
-            alt="Beleaf Logo"
-            width={40}
-            height={40}
-            priority
-          />
-          <span className={styles['category-collection__category']}>
-            Juweliere
-          </span>
-        </li>
-        <li className={styles['category-collection__item']}>
-          <Image
-            src="/images/language.svg"
-            alt="Beleaf Logo"
-            width={40}
-            height={40}
-            priority
-          />
-          <span className={styles['category-collection__category']}>
-            Second Hand
-          </span>
-        </li>
-        <li className={styles['category-collection__item']}>
-          <Image
-            src="/images/language.svg"
-            alt="Beleaf Logo"
-            width={40}
-            height={40}
-            priority
-          />
-          <span className={styles['category-collection__category']}>Möbel</span>
-        </li>
-        <li className={styles['category-collection__item']}>
-          <Image
-            src="/images/language.svg"
-            alt="Beleaf Logo"
-            width={40}
-            height={40}
-            priority
-          />
-          <span className={styles['category-collection__category']}>
-            Reisen
-          </span>
-        </li>
+        {categories.map((category) => (
+          <li className={styles['category-collection__item']} key={category.id}>
+            <Image
+              src={`https://robohash.org/${
+                category.i18n_slug
+              }?set=set2&size=${40}x${40}`}
+              alt={t(category.i18n_slug)}
+              width={40}
+              height={40}
+              priority
+            />
+            <span className={styles['category-collection__category']}>
+              {t(category.i18n_slug)}
+            </span>
+          </li>
+        ))}
       </ul>
     </div>
   );
