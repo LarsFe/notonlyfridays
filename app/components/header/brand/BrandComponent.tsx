4e99b3bf4203ce9a2f55fa10a9442ee69e79ced1
Lars Feske <l.feske@live.com>
2023-06-29 21:59:01 +0200

    Add link to Brand component

--- app/components/header/brand/BrandComponent.tsx
+++ app/components/header/brand/BrandComponent.tsx
@@ -1,13 +1,16 @@
+import Link from 'next/link';
 import Image from 'next/image';
 
 export default function BrandComponent() {
   return (
-    <Image
-      src="/images/beleaf.png"
-      alt="Beleaf Logo"
-      width={22}
-      height={40}
-      priority
-    />
+    <Link href={'/'}>
+      <Image
+        src="/images/beleaf.png"
+        alt="Beleaf Logo"
+        width={22}
+        height={40}
+        priority
+      />
+    </Link>
   );
 }
